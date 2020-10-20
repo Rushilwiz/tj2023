0576e8ca5357fec5352203fdeb8297ace29cc987
Rushil Umaretiya <2023rumareti@tjhsst.edu>
2020-10-20 04:40:53 -0400

    added whitenoise support for staticfiles

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,5 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
