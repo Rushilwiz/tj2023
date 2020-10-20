516406e48daeea1adecd68785042241350874938
Rushil Umaretiya <2023rumareti@tjhsst.edu>
2020-10-20 15:15:54 -0400

    fixed spacing and anim

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', context={'animate': True})
 
 
 def council(request):
