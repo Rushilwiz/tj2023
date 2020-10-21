207460bbf745a3500984751d5e60654ecbd204bc
Rushil Umaretiya <2023rumareti@tjhsst.edu>
2020-10-21 15:35:07 -0400

    added name for story

--- pages/models.py
+++ pages/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Story(models.Model):
-    header = models.CharField(max_length=30)
+    header = models.CharField(max_length=50)
     img_name = models.CharField(max_length=50, default=None, null=True)
     body = models.TextField(max_length=200)
     created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.header
